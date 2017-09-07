27e02b1b21948472d3f7e5ae1b67877ae6af0b9a
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-09-07 12:20:58 -0500

    up dijktra

--- dijktras_algorithm.py
+++ dijktras_algorithm.py
@@ -39,31 +39,31 @@ print('Parents => {0}'.format(parents))
 '''
 processed = []
 '''
-    The Function
-'''
-def find_lowest_cost_node(costs):
-    lowest_cost = float('inf')
-    lowest_cost_node = None
-    for node in costs:
-        cost = costs[node]
-        if cost < lowest_cost and node not in processed:
-            lowest_cost = cost
-            lowest_cost_node = node
-    return lowest_cost_node
-'''
     The Dijktra's algorithm
 '''
-node = find_lowest_cost_node(costs)
-while node is not None:
-    cost = costs[node]
-    neighbors = graph[node]
-    for n in neighbors.keys():
-        new_cost = cost + neighbors[n]
-        if costs[n] > new_cost:
-            costs[n] = new_cost
-            parents[n] = node
-    processed.append(node)
-    node = find_lowest_cost_node(costs)
+def Dijktra(graph,costs,parents):
+    def find_lowest_cost_node(costs):
+        lowest_cost = float('inf')
+        lowest_cost_node = None
+        for node in costs:
+            cost = costs[node]
+            if cost < lowest_cost and node not in processed:
+                lowest_cost = cost
+                lowest_cost_node = node
+        return lowest_cost_node
 
-    
-print('Optimized route => {0}'.format(parents))
+    node = find_lowest_cost_node(costs)
+    while node is not None:
+        cost = costs[node]
+        neighbors = graph[node]
+        for n in neighbors.keys():
+            new_cost = cost + neighbors[n]
+            if costs[n] > new_cost:
+                costs[n] = new_cost
+                parents[n] = node
+        processed.append(node)
+        node = find_lowest_cost_node(costs)
+    return parents
+
+
+print('Optimized route => {0}'.format(Dijktra(graph,costs,parents)))
