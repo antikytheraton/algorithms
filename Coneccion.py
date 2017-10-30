a521e5a8dea021ae8a8d0bcb0d00da90e3095a0c
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-10-30 11:21:49 -0600

    last update

---
+++ Coneccion.py
@@ -0,0 +1,13 @@
+'''
+    ATENUACION EN CABLES
+'''
+longitud_L1 = 60
+at_136 = 13.14
+atenuacion = (longitud_L1 * at_136)/100
+print(atenuacion)
+'''
+    GRAFO
+'''
+graph = {}
+graph['START'] = {}
+graph['START'] = {'A':longitud_L1}
